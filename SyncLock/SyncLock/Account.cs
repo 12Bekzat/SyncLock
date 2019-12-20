a90ddb8fc5bfbac9c2341e1eec87b7773cfefe76
Toleutai Bekzat <toleutaib@gmail.com>
2019-12-20 20:08:04 +0600

    Сделал схему хранения данных, и защитил данные от искажения

---
+++ SyncLock/SyncLock/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace SyncLock
+{
+    public class Account
+    {
+        private object syncObject = new object();
+
+        public int Sum { get; set; } = 1000;
+        public string Name { get; set; }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void Process(object sum)
+        {
+            Sum += (int)sum;
+            Console.WriteLine(@"{0} {1}-а {2} {3}, итог: {4}", (int)sum > 0 ? "К счету" : "Со счета", Name, (int)sum > 0 ? "пополнено" : "снято", sum, Sum);
+        }
+    }
+}
---
+++ SyncLock/SyncLock/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SyncLock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accounts = new List<Account>{
+            new Account{Name = "Бекзат"},
+            new Account{Name = "Алихан"},
+            new Account{Name = "Борис"},
+            new Account{Name = "Наиль"}
+            };
+            for (int i = 0; i < 1000; i++)
+            {
+                int chooise = new Random().Next(0, 2);
+                int index = new Random().Next(0, 4);
+                if (chooise == 0)
+                {
+
+                    ThreadPool.QueueUserWorkItem(accounts[index].Process, 100);
+                }
+                else if (chooise == 1)
+                {
+                    ThreadPool.QueueUserWorkItem(accounts[index].Process, -100);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
